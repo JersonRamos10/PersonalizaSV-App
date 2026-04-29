8cef8668dcd3fd9ec8b756965881f91bc026750a
JohanaLainez <johanalainez000@gmail.com>
2026-04-28 19:57:23 -0600

    Login y Dashboard

--- app/src/main/java/com/personalizasv/app/MainActivity.kt
+++ app/src/main/java/com/personalizasv/app/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.personalizasv.app
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -9,8 +11,25 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val editEmail = findViewById<EditText>(R.id.editEmail)
+        val editPassword = findViewById<EditText>(R.id.editPassword)
+        val btnIngresar = findViewById<Button>(R.id.editIngresar)
+
+        btnIngresar.setOnClickListener {
+            val email = editEmail.text.toString().trim()
+            val password = editPassword.text.toString().trim()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Complete todos los campos", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = android.content.Intent(this, DashboardActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
